45e710cc13c8333b14eac97f524c37674a0caef0
Tamil <tamil.ts@gmail.com>
2026-01-29 18:24:12 -0800

    config-server added

--- deployment/cloud/infrastructure/modules/cicd/variable.tf
+++ deployment/cloud/infrastructure/modules/cicd/variable.tf
@@ -1,19 +1,24 @@
 locals {
   services = {
     gateway-service = {
-      name = "gateway-service"
+      name   = "gateway-service"
       branch = "master"
-      repo = "btamilselvan/k8"
+      repo   = "btamilselvan/k8"
     }
     person-service = {
-      name = "person-service"
+      name   = "person-service"
       branch = "master"
-      repo = "btamilselvan/k8"
+      repo   = "btamilselvan/k8"
     }
     address-service = {
-      name = "address-service"
+      name   = "address-service"
       branch = "master"
-      repo = "btamilselvan/k8"
+      repo   = "btamilselvan/k8"
+    }
+    cloud-config-server = {
+      name   = "cloud-config-server"
+      branch = "master"
+      repo   = "btamilselvan/k8"
     }
   }
 }
